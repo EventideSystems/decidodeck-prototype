7e963be656bec12af2e0db691927f6f04b51b006
Tom Tuddenham <ferrisoxide@gmail.com>
2025-07-03 18:58:17 +0930

    added landing page logo

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  layout :determine_layout
+
+  def index
+    if user_signed_in?
+      render "home/index"
+    else
+      render "home/landing"
+    end
+  end
+
+  private
+
+  def determine_layout
+    # user_signed_in? ? "application" : "landing"
+    "landing"
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def html_lang
+    I18n.locale == I18n.default_locale ? "en" : I18n.locale.to_s
+  end
 end
