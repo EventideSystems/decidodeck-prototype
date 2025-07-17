ba5ed8941e0e16e1b383ed68fb6f08372b1a37dd
Tom Tuddenham <ferrisoxide@gmail.com>
2025-07-17 17:47:39 +0930

    added first cut of artifact editing

--- app/controllers/artifacts_controller.rb
+++ app/controllers/artifacts_controller.rb
@@ -1,7 +1,7 @@
 class ArtifactsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_workspace
-  before_action :set_artifact, only: [:show, :edit, :update, :destroy]
+  before_action :set_artifact, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @artifacts = @workspace.artifacts.includes(:content)
@@ -12,40 +12,40 @@ class ArtifactsController < ApplicationController
   end
 
   def new
-    @content_type = params[:content_type] || 'ArtifactContent::Note'
+    @content_type = params[:content_type] || "ArtifactContent::Note"
     @artifact = @workspace.artifacts.build
-    
+
     case @content_type
-    when 'ArtifactContent::Note'
+    when "ArtifactContent::Note"
       @content = ArtifactContent::Note.new
     else
-      redirect_to workspace_artifacts_path(@workspace), alert: 'Invalid content type'
-      return
+      redirect_to workspace_artifacts_path(@workspace), alert: "Invalid content type"
+      nil
     end
   end
 
   def create
-    @content_type = params[:content_type] || 'ArtifactContent::Note'
-    
+    @content_type = params[:content_type] || "ArtifactContent::Note"
+
     ActiveRecord::Base.transaction do
       case @content_type
-      when 'ArtifactContent::Note'
+      when "ArtifactContent::Note"
         @content = ArtifactContent::Note.new(note_params)
         @content.owner = current_user
         @content.save!
-        
+
         @artifact = @workspace.artifacts.build(artifact_params)
         @artifact.content = @content
         @artifact.save!
-        
-        redirect_to [@workspace, @artifact], notice: 'Artifact was successfully created.'
+
+        redirect_to [ @workspace, @artifact ], notice: "Artifact was successfully created."
       else
-        redirect_to workspace_artifacts_path(@workspace), alert: 'Invalid content type'
+        redirect_to workspace_artifacts_path(@workspace), alert: "Invalid content type"
       end
     end
   rescue ActiveRecord::RecordInvalid
     case @content_type
-    when 'ArtifactContent::Note'
+    when "ArtifactContent::Note"
       @content = ArtifactContent::Note.new(note_params)
     end
     @artifact = @workspace.artifacts.build(artifact_params)
@@ -62,9 +62,9 @@ class ArtifactsController < ApplicationController
       when ArtifactContent::Note
         @artifact.content.update!(note_params)
         @artifact.update!(artifact_params)
-        redirect_to [@workspace, @artifact], notice: 'Artifact was successfully updated.'
+        redirect_to [ @workspace, @artifact ], notice: "Artifact was successfully updated."
       else
-        redirect_to [@workspace, @artifact], alert: 'Unsupported content type for editing'
+        redirect_to [ @workspace, @artifact ], alert: "Unsupported content type for editing"
       end
     end
   rescue ActiveRecord::RecordInvalid
@@ -73,7 +73,7 @@ class ArtifactsController < ApplicationController
 
   def destroy
     @artifact.discard
-    redirect_to workspace_artifacts_path(@workspace), notice: 'Artifact was successfully deleted.'
+    redirect_to workspace_artifacts_path(@workspace), notice: "Artifact was successfully deleted."
   end
 
   private
@@ -87,7 +87,7 @@ class ArtifactsController < ApplicationController
   end
 
   def artifact_params
-    params.require(:artifact).permit(:tags => [])
+    params.require(:artifact).permit(tags: [])
   end
 
   def note_params
